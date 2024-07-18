1b4f363aeeb2ec93a1d841be0a4995951e2864fd
ezgiozturan <ezgiozturan05@gmail.com>
2024-07-18 13:35:38 +0300

    Dosya yapıları ve CSS ile ilgili düzenlemeler

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,23 @@
-import Home from "./components/jsx/Home";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+import "../src/components/css/App.css";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import React from "react";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
